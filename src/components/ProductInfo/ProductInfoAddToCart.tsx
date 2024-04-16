06cee026a51cd5d9d31e374764741a848d4af1c4
lutaok <lucadig.1994@gmail.com>
2024-04-16 08:17:04 +0200

    feature: add product removal on detail page

--- src/components/ProductInfo/ProductInfoAddToCart.tsx
+++ src/components/ProductInfo/ProductInfoAddToCart.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { MouseEvent } from "react";
+import { MouseEvent, useMemo } from "react";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import Icon from "@mui/material/Icon";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
 import { Product } from "@/interfaces/products";
+import { addProductToCart, removeProductFromCart } from "@/utils/cart";
 import { useCartContext, useCartDispatcherContext } from "../UserCartProvider/UserCartProvider";
-import { addProductToCart } from "@/utils/cart";
 
 interface ProductInfoAddToCartProps {
   product: Product;
@@ -14,6 +18,21 @@ interface ProductInfoAddToCartProps {
 const ProductInfoAddToCart = ({ product }: ProductInfoAddToCartProps) => {
   const cart = useCartContext();
   const setCart = useCartDispatcherContext();
+
+  const handleRemoveFromCartButtonClick = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    let freshCart;
+    try {
+      if (cart) {
+        freshCart = await removeProductFromCart(cart.products, product, cart.id);
+      } else {
+        throw new Error("no cart found");
+      }
+      setCart(freshCart);
+    } catch (e) {
+      console.error("Alert", e);
+    }
+  };
 
   const handleAddToCartButtonClick = async (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
@@ -29,6 +48,73 @@ const ProductInfoAddToCart = ({ product }: ProductInfoAddToCartProps) => {
       console.error("Alert", e);
     }
   };
+
+  const productInCart = useMemo(() => cart?.products.find((item) => item.id === product.id), [cart, product]);
+
+  if (productInCart) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.25rem",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "0.825rem",
+            textTransform: "unset",
+          }}
+        >
+          Add to cart
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "start",
+            gap: "0.75rem",
+          }}
+        >
+          <Button
+            sx={{
+              color: "white",
+              lineHeight: 1,
+              padding: "0rem",
+              minWidth: "unset",
+            }}
+            onClick={handleRemoveFromCartButtonClick}
+          >
+            <Icon>
+              <RemoveCircleOutlineIcon
+                sx={{
+                  width: "1rem",
+                }}
+              />
+            </Icon>
+          </Button>
+          <Typography>{productInCart.quantity}</Typography>
+          <Button
+            sx={{
+              color: "white",
+              lineHeight: 1,
+              padding: "0rem",
+              minWidth: "unset",
+            }}
+            onClick={handleAddToCartButtonClick}
+          >
+            <Icon>
+              <AddCircleOutlineIcon
+                sx={{
+                  width: "1rem",
+                }}
+              />
+            </Icon>
+          </Button>
+        </Box>
+      </Box>
+    );
+  }
 
   return (
     <Button
